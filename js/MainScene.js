db0ab13ed6a026732831deedb73127b97472d3c3
cjgonza2 <camrong520@gmail.com>
2022-11-17 16:51:11 -0500

    Added score to game. it will tick up; also enemies move towards the player.

--- js/MainScene.js
+++ js/MainScene.js
@@ -2,6 +2,14 @@
 var cursors;
 var player;
 var enemy;
+var enemy2;
+let score = 0
+var scoreTxt;
+//seems like theres three ways to declare a variable in 3 ways
+//1. let x...
+//2. var x...
+//3. const x...
+
 
 export default class MainScene extends Phaser.Scene{ //inherits Phaser.SCene Class.
     constructor (){//Whenever we make a class we need a constructor?
@@ -28,12 +36,33 @@ export default class MainScene extends Phaser.Scene{ //inherits Phaser.SCene Cla
         //???
         //player.setColliderWorldBounds(true);
 
+        //sets a constant variable "stats"
+        //I can't just set constant, it needs to have something assigned to it
+        //i might be able to try to set it to a value but im not sure how I would 
+        //minipulate the data. 
+        //const stats = this.add.image(810, 0, 'Button');
+        //stats.setDataEnabled();
+
+        //stats.data.set('score', 0);
+
+        player.physics.add.collider(player, enemy);
+        //this.physics.add.collider(player, enemy2);
+
+        scoreTxt = this.add.text(350, 270, 'Score: 0', {font: '32px pressStart', fill: '#00ff00'});
+
+
+
+
+
         cursors = this.input.keyboard.createCursorKeys();
         player = this.physics.add.image(400,400,'paladin');
         player.setCollideWorldBounds(true);
 
         enemy = this.physics.add.sprite(600, 500, 'hellKnight');
         enemy.setCollideWorldBounds(true);
+
+        enemy2 = this.physics.add.sprite(100, 100, 'hellKnight'); 
+        enemy2.setCollideWorldBounds(true);
     
         //in this situation player variable is local.
         //var player = this.add.image (500, 500, 'paladin');
@@ -47,8 +76,10 @@ export default class MainScene extends Phaser.Scene{ //inherits Phaser.SCene Cla
     //learning from scratch, good directions.
     update()
     {
+        
         player.setVelocity(0);
         enemy.setVelocity(0);
+        enemy2.setVelocity(0);
 
         if(cursors.left.isDown)
         {
@@ -67,6 +98,8 @@ export default class MainScene extends Phaser.Scene{ //inherits Phaser.SCene Cla
             player.setVelocityY(300);
         }
 
+        score += 1;
+        scoreTxt.setText('Score: ' + score);
         
 
         this.enemyFollows();
@@ -74,8 +107,11 @@ export default class MainScene extends Phaser.Scene{ //inherits Phaser.SCene Cla
 
     enemyFollows()
     {
-       enemy.x = player.body.position.x;
-       enemy.y = player.body.position.y;
+
+        //.moveToObject is a handy function to move an object to another
+        //seems good for having an enemy 
+        this.physics.moveToObject(enemy, player, 150);
+        this.physics.moveToObject(enemy2, player, 50)
        //if i've already declared a variable
        //ie var enemy. I can't just say this.enemy, 
        //since in that case it's not defined!
